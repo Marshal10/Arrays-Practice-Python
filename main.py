62a557a00ba274b2c2853b2b5aaca66d98cbf053
Marshal10 <marshalmaleane123@gmail.com>
2024-01-26 15:47:50 +0530

    Access the elements

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from array import *
 
 #1.Create an array and traverse
+print("Step 1")
 arr1=array('i',[1,2,3,4,5])
 for num in arr1:
     print(num)
+    
+# 2. Access individual elements through index
+print("Step 2")
+print(arr1[2])
